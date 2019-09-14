052ae9a74cf6a01fef90523362bb502698daf3d9
Sue Liu <sue.liu@mudano.com>
2019-09-14 13:17:16 +0100

    Work on customer complaints

--- preprocess/gen_model_data.py
+++ preprocess/gen_model_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import spacy
 nlp = spacy.load("en_core_web_sm")
 
-from preprocess import TextClean
+from preprocess.clean_text import TextClean
 
 
 def text_preprocess(text):
@@ -28,7 +28,12 @@ def gen_model_data(df, subset=None, features=None, lowercase=True, min_sentence_
     cleaned_text, idx = cleaner.fit_transform(text, return_indices=True)
     df = df.iloc[idx, :]
     df['cleaned_text'] = cleaned_text
+
+    # Remove the extremely long winded people
+    df = df[df['cleaned_text'].apply(lambda x: len(str(x)) < 1000)]
+
     df['text'] = df['cleaned_text'].apply(text_preprocess)
+    df.drop(columns=['cleaned_text'], inplace=True)
 
     return df
 
@@ -37,7 +42,7 @@ if __name__ == '__main__':
     complaints_file = '../data/Consumer_Complaints.csv'
     df = pd.read_csv(complaints_file, low_memory=False)
 
-    features = ['Date received', 'Product', 'Issue', 
+    features = ['Date received', 'Product', 'Sub-product', 'Issue', 'Sub-issue', 
        'Consumer complaint narrative', 'Company public response', 'Company',
        'Submitted via', 'Date sent to company', 'Company response to consumer',
        'Timely response?', 'Consumer disputed?', 'Complaint ID']
@@ -46,4 +51,4 @@ if __name__ == '__main__':
         'Consumer complaint narrative']
 
     valid_df = gen_model_data(df, subset=nonan_subset, features=features)
-    valid_df.to_csv('../data/cleaned_text.csv', index=False)
+    valid_df.to_csv('../data/processed_text.csv', index=False)
