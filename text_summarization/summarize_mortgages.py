a81eda74a6b07c59460885dd4dc2278687ade8f3
Sue Liu <sue.liu@mudano.com>
2019-10-19 16:52:56 +0100

    Fixed bug in text summarization code

--- text_summarization/summarize_mortgages.py
+++ text_summarization/summarize_mortgages.py
@@ -13,13 +13,14 @@ if module_path not in sys.path:
     sys.path.append(module_path)
 
 from preprocess.clean_and_tokenize import tokenize_sentences, clean_and_tokenize
-from scripts.text_summarize_textrank import gen_sent_vectors, compute_similarity_matrix, rank_sentences, gen_top_n_sentences
-
+from text_summarization.text_summarize_textrank import gen_sent_vectors,\
+                compute_similarity_matrix, rank_sentences, gen_top_n_sentences
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/product_merged.csv', usecols=['Complaint ID', 'Product', 'Issue',
-                                                       'Consumer complaint narrative'])
+    df = pd.read_csv('../data/product_merged.csv', 
+        usecols=['Complaint ID', 'Product', 'Issue', 'Consumer complaint narrative'])
+    
     subset = df[df.Product == 'Mortgage']
     
     sentences = tokenize_sentences(subset['Consumer complaint narrative'])
@@ -28,7 +29,7 @@ if __name__ == '__main__':
     
     # Extract word vectors
     word_embeddings = {}
-    f = open('../ipynb/glove.6B.100d.txt', encoding='utf-8')
+    f = open('../data/embeddings/glove.6B.100d.txt', encoding='utf-8')
     for line in f:
         values = line.split()
         word = values[0]
@@ -37,11 +38,13 @@ if __name__ == '__main__':
     f.close()
     
     
-    data = {'sentences': sentences, 'clean_sentences': clean_sentences, 'embeddings': word_embeddings, 
+    data = {'sentences': sentences, 'clean_sentences': clean_sentences, 
+            'embeddings': word_embeddings, 
             'complaint_id': subset['Complaint ID'].values}
-    with open('../output/mortgage_sentences.pickle', 'wb') as f:
+
+    with open('results/mortgage_sentences.pickle', 'wb') as f:
         pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-        
+
     summarized = [gen_top_n_sentences(sentences[idx], 
                                       clean_sentences[idx], 
                                       word_embeddings) for idx in range(len(subset))]
@@ -50,4 +53,4 @@ if __name__ == '__main__':
     subset['summarized'] = summarized
     subset['cleaned_summarized'] = cleaned_summarized
     
-    subset.to_csv('../data/mortgage_with_summaries.csv', index=False)
+    subset.to_csv('results/mortgage_with_summaries.csv', index=False)
--- text_summarization/text_summarize_textrank.py
+++ text_summarization/text_summarize_textrank.py
@@ -4,6 +4,7 @@ import numpy as np
 import networkx as nx
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 def gen_sent_vectors(clean_sentences, word_embeddings):
     """
     Generate sentence vectors using word embeddings.
@@ -11,7 +12,8 @@ def gen_sent_vectors(clean_sentences, word_embeddings):
     sentence_vectors = []
     for i in clean_sentences:
         if len(i) != 0:
-            v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
+            v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(
+                len(i.split())+0.001)
         else:
             v = np.zeros((100,))
         sentence_vectors.append(v)
@@ -32,14 +34,17 @@ def compute_similarity_matrix(one_complaint, sentence_vectors):
 
 
 def rank_sentences(sim_mat, one_complaint, maxlen):
-    """Rank sentences using their PageRank score"""
+    """
+    Rank sentences using their PageRank score
+    """
     nx_graph = nx.from_numpy_array(sim_mat)
     try: # Note that PageRank is not guaranteed to converge, hence return the same sentences
         scores = nx.pagerank(nx_graph, max_iter=100)
     except Exception:
         scores = np.zeros(len(one_complaint))
     
-    ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(one_complaint)), reverse=True)
+    ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(one_complaint)), \
+        reverse=True)
     return ranked_sentences
 
 
@@ -61,18 +66,3 @@ def gen_top_n_sentences(one_complaint, clean_sentences, word_embeddings, maxlen=
         
         condensed = ' '.join(re.sub('xx\\S+|XX\\S+', ' ', ' '.join(condensed_sent)).split())
     return condensed
-
-if __name__ == '__main__':
-    with open('../output/sentences.pickle', 'rb') as f:
-        data = pickle.load(f)
-
-    sentences = data['sentences']
-    clean_sentences = data['clean_sentences']
-    word_embeddings = data['embeddings']
-
-    with open('../output/summary_300000.txt', 'w') as f:
-        for idx in range(300000, len(sentences)):
-            if idx % 200 == 0:
-                print(idx)
-            summary = gen_top_n_sentences(sentences[idx], clean_sentences[idx], maxlen=10)
-            f.write('{}\n'.format(summary))
