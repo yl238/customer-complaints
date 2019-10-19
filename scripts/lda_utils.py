dbcc7a73d543c0d9672645cbda0e16fe91f23962
Sue Liu <sue.liu@mudano.com>
2019-10-19 11:18:52 +0100

    Refactored and deleted scripts directory

--- scripts/lda_utils.py
+++
@@ -1,7 +0,0 @@
-def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        message = "Topic #%d: " % topic_idx
-        message += ",".join([feature_names[i]
-                             for i in topic.argsort()[:-n_top_words - 1:-1]])
-        print(message)
-    print()
